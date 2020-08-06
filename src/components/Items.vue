2f35214d210997704e2249c24ac809933544e932
Jessica <jessica.francavilla86@gmail.com>
2020-08-06 17:06:29 -0300

    add API call

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -4,7 +4,7 @@
       <img :src="item.photo" height="148px" width="148px" />
       <p class="name">{{item.name}}</p>
       <p class="price">$ {{item.price}}</p>
-      <button class="btn">Agregar al Carrito</button>
+      <button class="btn secondary">Agregar al Carrito</button>
     </div>
   </div>
 </template>
@@ -35,36 +35,32 @@ export default {
 }
 
 div .card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
   background-color: #ffffff;
   border-radius: 3px;
   box-shadow: 5px 5px 5px 0px #00000008 ;
   margin-bottom: 30px;
+  margin-right: 2px;;
   padding: 10px;
   width: 180px;
+  height: auto;
 }
 
-.btn {
+.secondary {
   background: #ffffff;
   border: 1px solid #0070e0;
-  border-radius: 3px;
   color: #0070e0;
-  font-size: 13px;
-  height: 32px;
-  outline: none;
-  text-align: center;
-  width: 132px;
-}
+  }
 
-.btn:hover {
-  background-color: #378de5;
+.secondary:hover {
+  background-color: #0070e0;
   border: 1px solid transparent;
   color: #ffffff;
 }
 
-.btn:active {
-  position: relative;
-  top: 1px;
-}
 </style>
 
 
---
+++ src/components/LoadMore.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="btn primary" v-on:click="loadMore">Cargar Más</button>
+
+</template>
+
+<script>
+import Items from './Items';
+export default {
+    name: "LoadMore",
+    components: Items,
+    methods: {
+        loadMore() {
+            
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
